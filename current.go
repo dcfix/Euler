812f6f5326fe91d841e4029baa32e0de5d161eec
Darren Fix <darren.fix@gmail.com>
2015-10-15 21:36:36 -0600

    added solution three

--- current.go
+++ current.go
@@ -1,20 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
-	"strings"
+	"math"
+	"os"
+	"strconv"
 )
 
 func main() {
 
-	content, err := ioutil.ReadFile("primes.txt")
-
+	primes, err := getPrimes("primes.txt")
 	if err != nil {
-		//Do something
+		fmt.Println("Error loading pre calculate prime numbers")
 	}
-	primes := strings.Split(string(content), "\n")
+	fmt.Println("got my primes")
+	fmt.Println(primes[2:5])
 
-	answer := 0
+	target := 600851475143
+	highest := 0
+	square := int(math.Sqrt(float64(target)))
 
+	for _, p := range primes {
+		if p < square {
+			if target%p == 0 {
+				highest = p
+			}
+
+		}
+
+	}
+	fmt.Println(highest)
+}
+
+func getPrimes(path string) ([]int, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var myPrimes []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+		myPrimes = append(myPrimes, val)
+	}
+	return myPrimes, scanner.Err()
 }
